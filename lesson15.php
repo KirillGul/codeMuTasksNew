f922dedd46a1de02fe4b5d0ff0ac2daec942c543
Kirill <guliev.kirill@eyandex.ru>
2021-04-01 20:11:04 +0300

    Готовый урок 15

--- lesson15.php
+++ lesson15.php
@@ -1,3 +1,41 @@
 <?php echo 'Задача №1 --- Решение:<br>';
+$str = "";
+for ($i=1; $i<=9; $i++) $str .= $i;
+echo $str;
+echo '<br><br>'; ?>
 
+<?php echo 'Задача №2 --- Решение:<br>';
+$str = "";
+for ($i=9; $i>=1; $i--) $str .= $i;
+echo $str;
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №3 --- Решение:<br>';
+$str = "-";
+for ($i=1; $i<=9; $i++) $str .= $i."-";
+echo $str;
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №4 --- Решение:<br>';
+$str = "";
+for ($i=1; $i<=20; $i++) {
+    echo $str .= "x";
+    echo "<br>";
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №5 --- Решение:<br>';
+for ($i=1; $i<=9; $i++) {
+    for ($j=1; $j<=$i; $j++) {
+        echo $i;        
+    }
+    echo "<br>";
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №6 --- Решение:<br>';
+for ($i=1; $i<=5; $i++) {
+    echo str_repeat('x', $i*2);
+    echo "<br>";
+}
 echo '<br><br>'; ?>
