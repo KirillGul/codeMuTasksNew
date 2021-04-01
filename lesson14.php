8f6ff31e34fc4841f5e0235ec235d5010e8cfb5a
KirillGul <guliev.kirill@yandex.ru>
2021-04-01 17:52:48 +0300

    Готовй урок 14

--- lesson14.php
+++ lesson14.php
@@ -1,3 +1,31 @@
 <?php echo 'Задача №1 --- Решение:<br>';
+$int1 = 2;
+$int2 = 3;
 
+sravni($int1, $int2);
+
+function sravni ($i1, $i2) {
+   return $i1 == $i2;
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №2 --- Решение:<br>';
+$int1 = 5;
+$int2 = 6;
+
+echo sravni2($int1, $int2);
+
+function sravni2 ($i1, $i2) {
+   return $i1+$i2 > 10;
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №3 --- Решение:<br>';
+$int = -5;
+
+echo fPlusMinus($int);
+
+function fPlusMinus($i) {
+   return $i<0;
+}
 echo '<br><br>'; ?>
