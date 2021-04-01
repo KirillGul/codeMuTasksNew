024a8989f419f00c0528c35ce829b505c539993a
KirillGul <guliev.kirill@yandex.ru>
2021-04-01 17:09:02 +0300

    Готовый урок 12

--- lesson12.php
+++ lesson12.php
@@ -1,3 +1,32 @@
 <?php echo 'Задача №1 --- Решение:<br>';
+echo kvadrat(2);
 
+function kvadrat ($num) {
+   return $num*$num;
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №2 --- Решение:<br>';
+echo int_sum(2, 3);
+
+function int_sum ($num1, $num2) {
+   return $num1+$num2;
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №3 --- Решение:<br>';
+echo f3(3, 2, 2);
+
+function f3 ($num1, $num2, $num3) {
+   return ($num1-$num2)/$num3;
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №4 --- Решение:<br>';
+echo f4Week(5);
+
+function f4Week ($num1) {
+   $week = [1=>'пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+   return $week[$num1];
+}
 echo '<br><br>'; ?>
