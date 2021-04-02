53aff33e02585f81ea2095c0b7c0216a943032c9
Kirill <guliev.kirill@eyandex.ru>
2021-04-02 18:50:44 +0300

    Готовый урок 18

--- lesson18.php
+++ lesson18.php
@@ -17,6 +17,40 @@ echo '<br><br>'; ?>
 
 <?php echo 'Задача №2 --- Решение:<br>';
 function isSklonenie ($int, $str1, $str2, $str3) {
-   
+   if ($int == 1) return $int." ".$str1;
+   elseif ($int >= 2 && $int <= 4) return $int." ".$str2;
+   elseif ($int >= 5) return $int." ".$str3;
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №3 --- Решение:<br>';
+function isTicket ($int) {
+   return $int[0]+$int[1]+$int[2] == $int[3]+$int[4]+$int[5];
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №3 --- Решение:<br>';
+for ($i=0; $i<=1000; $i++) {
+   $arr[$i] = getSumArray(getDivisors($i));
+}
+
+foreach ($arr as $key=>$value) {
+   for ($i=0; $i<=count($arr)-1; $i++) {
+      if ($value == $arr[$i] && $key != $i) {
+         echo $key.' '.$i."<br>";
+      }
+   }
+}
+
+function getDivisors ($int) {
+   $arr = [];
+   for($i=1; $i <= $int; $i++) {
+      if ($int % $i == 0) $arr[] = $i;
+   }
+   return $arr;
+}
+
+function getSumArray ($arr) {
+   return array_sum($arr);
 }
 echo '<br><br>'; ?>
