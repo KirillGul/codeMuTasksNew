11e9b4ad8ff68b7e4260ba51a095f69866c3784e
KirillGul <guliev.kirill@yandex.ru>
2021-04-02 16:34:09 +0300

    Не готовый урок 18

--- lesson18.php
+++ lesson18.php
@@ -1,3 +1,22 @@
 <?php echo 'Задача №1 --- Решение:<br>';
+function getTransit ($str) {
+   $arr = ['a'=>'a', 'б'=>'b', 'в'=>'v', 'г'=>'g', 
+      'д'=>'d', 'е'=>'e', 'ё'=>'e', 'ж'=>'zh', 'з'=>'z', 
+      'и'=>'i', 'й'=>'i', 'к'=>'k', 'л'=>'l', 'м'=>'m', 
+      'н'=>'n', 'о'=>'o', 'п'=>'p', 'р'=>'r', 'с'=>'s', 
+      'т'=>'t', 'у'=>'u', 'ф'=>'f', 'х'=>'kh', 'ц'=>'ts', 
+      'ч'=>'ch', 'ш'=>'sh', 'щ'=>'shch', 'ъ'=>'ie', 'ы'=>'y', 
+      'ь'=>'', 'э'=>'e', 'ю'=>'iu', 'я'=>'ia'];
 
+   foreach (str_split($str) as $value) {
+      $arrTranslit[] = $arr[$value]; 
+   }
+   return $arrTranslit;
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №2 --- Решение:<br>';
+function isSklonenie ($int, $str1, $str2, $str3) {
+   
+}
 echo '<br><br>'; ?>
