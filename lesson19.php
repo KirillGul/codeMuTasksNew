963e0ff4744f6c297e8f177fec08e8184e06ef41
Kirill <guliev.kirill@eyandex.ru>
2021-04-02 20:04:05 +0300

    Готовый урок 20

--- lesson19.php
+++ lesson19.php
@@ -1,3 +1,35 @@
 <?php echo 'Задача №1 --- Решение:<br>';
+$str = '12345678901011';
+$int = 3;
 
+echo cut($str, $int);
+echo "<br>";
+echo cut($str);
+
+function cut($str, $int=10) {
+    return substr($str, 0, $int);
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №2 --- Решение:<br>';
+$arr = [1,2,3,4,5,6];
+
+lastArray($arr);
+
+function lastArray($arr) {
+    echo array_shift($arr);
+    if (!empty($arr)) lastArray($arr);
+}
+echo '<br><br>'; ?>
+
+<?php echo 'Задача №3 --- Решение:<br>';
+$int = 123456545;
+
+intSumNine($int);
+
+function intSumNine($int) {
+    $i = array_sum(str_split($int));
+    if ($i > 9) intSumNine($i);
+    else echo $i;
+}
 echo '<br><br>'; ?>
