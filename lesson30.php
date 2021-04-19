2931bc655aaa29dbf53bcc272bbf6283b5a8d7fb
KirillGul <50946729+KirillGul@users.noreply.github.com>
2021-04-19 19:54:44 +0300

    Готовый урок 30

--- lesson30.php
+++ lesson30.php
@@ -2,5 +2,7 @@
 echo "Задачи урока находяться в файле .htaccess<br>";
 echo "lesson30.php<br>";
 
-//var_dump($_SERVER['REQUEST_URI']);
+var_dump($_SERVER['REQUEST_URI']);
+echo "<br>";
+var_dump($_GET['get']);
 ?>
